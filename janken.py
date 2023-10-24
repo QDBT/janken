9a3cc9d4bf0c8a05d38e44b198e3a49800a0bc79
Long Tuong <1cda2122@cc.u-tokai.ac.jp>
2023-10-24 11:59:51 +0900

    jankenを追加した

---
+++ janken.py
@@ -0,0 +1,6 @@
+name = input("Enter your name:")
+print("your name is " + name)
+print("1--Rock ; 2--Paper ; 3--Sister")
+for i in range(10):
+    push = int(input((str(i+1))+"回目:"))
+    print(push)

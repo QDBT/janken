44fdc4235832a2a077e6e98be656f1db2482bf6a
QDBT <1cda2122@cc.u-tokai.ac.jp>
2023-10-24 13:53:43 +0900

    結果判断の関数を追加した。

--- janken.py
+++ janken.py
@@ -1,6 +1,14 @@
+def result(user_choice, com):
+    if (user_choice == com):
+        print("tie")
+    elif ((user_choice == 1 and com == 3) or (user_choice == 2 and com == 1) or (user_choice == 3 and com == 2)):
+        print(name + " win")
+    else:
+        print(name + " lose")
+
+
 name = input("Enter your name:")
 print("your name is " + name)
-
 user = []
 com = [1, 2, 1, 3, 1]
 print("1--Rock ; 2--Paper ; 3--Sister")
@@ -8,9 +16,4 @@ for i in range(5):
     user_choice = int(input((str(i+1))+"回目:"))
     user.append(user_choice)
     print(user_choice)
-    if (user_choice == com[i]):
-        print("tie")
-    elif ((user_choice == 1 and com[i] == 3) or (user_choice == 2 and com[i] == 1) or (user_choice == 3 and com[i] == 2)):
-        print("user win")
-    else:
-        print("user lose")
+    result(user[i], com[i])
